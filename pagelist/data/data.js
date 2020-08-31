70d4eebebcdd238017d1486ae7a7b8bf38d40857
choi hyun won <danboys@domfam.com>
2020-08-31 13:46:05 +0900

    init

---
+++ pagelist/data/data.js
@@ -0,0 +1,346 @@
+var data = {}
+data.common = {
+  'title': '공통',
+  'list': [
+    {
+      'stateClass': 'complete',
+      'url': 'LAYOUT',
+      'category': 'common',
+      'page': 'LAYOUT'
+    },
+    {
+      'stateClass': 'complete',
+      'url': 'COM_GENERAL_01',
+      'category': 'common',
+      'page': 'GENERAL_01'
+    },
+    {
+      'stateClass': 'complete',
+      'url': 'COM_GENERAL_02',
+      'category': 'common',
+      'page': 'GENERAL_02'
+    },
+    {
+      'stateClass': 'complete',
+      'url': 'COM_GENERAL_03',
+      'category': 'common',
+      'page': 'GENERAL_03'
+    },
+    {
+      'stateClass': 'complete',
+      'url': 'COM_modal_01',
+      'category': 'common',
+      'page': 'MODAL'
+    },
+    {
+      'stateClass': 'complete',
+      'url': 'COM_drawer_01',
+      'category': 'common',
+      'page': 'drawer'
+    },
+    {
+      'stateClass': 'complete',
+      'url': '00_common_01_profile_01',
+      'category': 'common',
+      'page': 'navigation'
+    },
+    {
+      'stateClass': 'complete',
+      'url': '00_common_01_popup_태그 알림 설정',
+      'category': 'common',
+      'page': '태그 알림 설정 팝업'
+    },
+    {
+      'stateClass': 'complete',
+      'url': '00_common_02_popup_언어설정',
+      'category': 'common',
+      'page': '언어 설정 팝업'
+    },
+    {
+      'stateClass': 'complete',
+      'url': '00_common_03_search_01_검색어 입력 전',
+      'category': 'common',
+      'page': '검색어 입력 전'
+    },
+    {
+      'stateClass': 'complete',
+      'url': '00_common_03_search_02_검색어 입력 시',
+      'category': 'common',
+      'page': '검색어 입력 시'
+    },
+    {
+      'stateClass': 'complete',
+      'url': '00_common_03_search_03_최근 검색어 없음',
+      'category': 'common',
+      'page': '최근 검색어 없음'
+    },
+    {
+      'stateClass': 'complete',
+      'url': '00_common_04_result_01_검색결과',
+      'category': 'common',
+      'page': '검색결과'
+    },
+    {
+      'stateClass': 'complete',
+      'url': '00_common_04_result_02_검색결과 없음',
+      'category': 'common',
+      'page': '검색결과 없음'
+    },
+    {
+      'stateClass': 'complete',
+      'url': '00_common_05_footer_01_이용약관',
+      'category': 'common',
+      'page': '이용약관'
+    },
+  ]
+}
+
+data.login = {
+  'title': '로그인',
+  'list': [
+
+    {
+      'stateClass': 'complete',
+      'url': '01_main_01_login',
+      'category': 'Main',
+      'page': '로그인'
+    },
+    {
+      'stateClass': 'complete',
+      'url': '01_main_02_id_02_인증번호 입력_01',
+      'category': 'Main',
+      'page': '인증번호 입력_01'
+    },
+    {
+      'stateClass': 'complete',
+      'url': '01_main_02_id_02_인증번호 입력_02',
+      'category': 'Main',
+      'page': '인증번호 입력_02'
+    },
+    {
+      'stateClass': 'complete',
+      'url': '01_main_02_id_02_인증번호 입력_03',
+      'category': 'Main',
+      'page': '인증번호 입력_03'
+    },
+    {
+      'stateClass': 'complete',
+      'url': '01_main_02_id_02_인증번호 입력_04',
+      'category': 'Main',
+      'page': '인증번호 입력_04'
+    },
+    {
+      'stateClass': 'complete',
+      'url': '01_main_02_id_02_인증번호 입력_05',
+      'category': 'Main',
+      'page': '인증번호 입력_05'
+    },
+    {
+      'stateClass': 'complete',
+      'url': '01_main_02_id_02_결과_01',
+      'category': 'Main',
+      'page': '아이디 찾기 결과_01'
+    },
+    {
+      'stateClass': 'complete',
+      'url': '01_main_02_id_02_결과_02',
+      'category': 'Main',
+      'page': '아이디 찾기 결과_02'
+    },
+    {
+      'stateClass': 'complete',
+      'url': '01_main_03_password_01_비밀번호 찾기',
+      'category': 'Main',
+      'page': '비밀번호 찾기'
+    },
+    {
+      'stateClass': 'complete',
+      'url': '01_main_03_password_02_인증번호 입력',
+      'category': 'Main',
+      'page': '비밀번호 인증번호 입력'
+    },
+    {
+      'stateClass': 'complete',
+      'url': '01_main_03_password_03_비밀번호 찾기 결과',
+      'category': 'Main',
+      'page': '비밀번호 찾기 결과'
+    },
+  ]
+}
+
+data.main = {
+  'title': '메인',
+  'list': [
+    {
+      'stateClass': 'complete',
+      'url': '00_common_01_profile_01',
+      'category': 'Main',
+      'page': '프로필'
+    },
+    {
+      'stateClass': 'complete',
+      'url': '01_main_04_추천_01_학부모',
+      'category': 'Main',
+      'page': '추천 (학부모)'
+    },
+    {
+      'stateClass': 'complete',
+      'url': '01_main_04_추천_01_자녀',
+      'category': 'Main',
+      'page': '추천 (자녀)'
+    },
+    {
+      'stateClass': 'complete',
+      'url': '01_main_04_추천_03_로그아웃',
+      'category': 'Main',
+      'page': '추천 (로그아웃)'
+    },
+    {
+      'stateClass': 'complete',
+      'url': '01_main_05_교육과정별 메인_01_초등_01',
+      'category': 'Main',
+      'page': '교육과정'
+    },
+    {
+      'stateClass': 'complete',
+      'url': '01_main_06_태그별 전체 목록',
+      'category': 'Main',
+      'page': '태그별 전체 목록'
+    }
+  ]
+}
+
+data.detail = {
+  'title': '상세',
+  'list': [
+    {
+      'stateClass': 'complete',
+      'url': '01_main_08_app detail_01_로그아웃',
+      'category': 'Main',
+      'page': '로그아웃'
+    },
+    {
+      'stateClass': 'complete',
+      'url': '01_main_08_app detail_01_학부모',
+      'category': 'Main',
+      'page': '학부모'
+    },
+    {
+      'stateClass': 'complete',
+      'url': '01_main_08_app detail_01_학생',
+      'category': 'Main',
+      'page': '학생'
+    },
+    {
+      'stateClass': 'complete',
+      'url': '01_main_08_app detail_01_학습소개_02_scroll',
+      'category': 'Main',
+      'page': '학습소개'
+    },
+    {
+      'stateClass': 'complete',
+      'url': '01_main_08_app detail_01_학습소개_로그아웃',
+      'category': 'Main',
+      'page': '학습소개(로그아웃)'
+    },
+    {
+      'stateClass': 'complete',
+      'url': '01_main_08_app detail_01_학습중',
+      'category': 'Main',
+      'page': '학습소개(탭메뉴)'
+    },
+    {
+      'stateClass': 'complete',
+      'url': '01_main_08_app detail_02_리뷰_01',
+      'category': 'Main',
+      'page': '리뷰(탭메뉴)'
+    },
+    {
+      'stateClass': 'complete',
+      'url': '01_main_08_app detail_03_관련 학습',
+      'category': 'Main',
+      'page': '관련학습(탭메뉴)'
+    },
+    {
+      'stateClass': 'complete',
+      'url': '01_main_09_popup_06_평가 및 리뷰 작성',
+      'category': 'Main',
+      'page': '평가하기(팝업)'
+    },
+
+
+
+  ]
+}
+
+data.event = {
+  'title': '이벤트',
+  'list': [
+    {
+      'stateClass': 'complete',
+      'url': '02_event_01_진행중인 이벤트',
+      'category': 'Event',
+      'page': '진행중인 이벤트'
+    },
+    {
+      'stateClass': 'complete',
+      'url': '02_event_01_진행중인 이벤트_02_이벤트 없음',
+      'category': 'Event',
+      'page': '진행중인 이벤트_이벤트 없음'
+    },
+    {
+      'stateClass': 'complete',
+      'url': '02_event_02_지난 이벤트',
+      'category': 'Event',
+      'page': '지난 이벤트'
+    },
+    {
+      'stateClass': 'complete',
+      'url': '02_event_02_지난 이벤트_02_이벤트 없음',
+      'category': 'Event',
+      'page': '지난 이벤트_이벤트 없음'
+    },
+  ]
+}
+
+// data.payment = {
+//   'title': '결제',
+//   'list': [
+//     {
+//       'stateClass': 'complete',
+//       'url': '01_main_10_app detail_결제_01_학습할 자녀 선택',
+//       'category': 'Payment',
+//       'page': '결제_학습할 자녀 선택'
+//     },
+//   ]
+// }
+
+data.cs = {
+  'title': '고객센터',
+  'list': [
+    {
+      'stateClass': 'complete',
+      'url': '03_cs_01_공지사항',
+      'category': 'CS',
+      'page': '공지사항'
+    },
+    {
+      'stateClass': 'complete',
+      'url': '03_cs_02_문의내역_문의내역없음',
+      'category': 'CS',
+      'page': '문의내역_없음'
+    },
+    {
+      'stateClass': 'complete',
+      'url': '03_cs_03_문의내역_로그인',
+      'category': 'CS',
+      'page': '문의내역_로그인'
+    },
+    {
+      'stateClass': 'complete',
+      'url': '03_cs_04_문의하기',
+      'category': 'CS',
+      'page': '문의하기'
+    },
+  ]
+}
