eb9b553400610cc4405ec18821dfd4f5f1434f48
Yujung Jung <darron@nate.com>
2020-09-01 17:42:07 +0900

    작업 진행중 저장용

--- gulpfile.js
+++ gulpfile.js
@@ -10,120 +10,121 @@ sass.compiler = require('node-sass');
 
 /* scss TASK*/
 function scss() {
-  return gulp.src('src/scss/**/*.scss')
-    // style.css 파일 하나로 할 경우
-    // .pipe(sass.sync().on('error', sass.logError))
-    // .pipe(combine())
-    // .pipe(concat('style.scss'))
+	return gulp.src('src/scss/**/*.scss')
+		// style.css 파일 하나로 할 경우
+		// .pipe(sass.sync().on('error', sass.logError))
+		// .pipe(combine())
+		// .pipe(concat('style.scss'))
 
-    .pipe(sass.sync().on('error', sass.logError))
-    .pipe(gulp.dest('./dist/css'))
+		.pipe(sass.sync().on('error', sass.logError))
+		.pipe(gulp.dest('./dist/css'))
 }
 
 function htmlInclude() {
-  let source = 'src/html/include/*.html';
-  return gulp.src(source)
-    .pipe(html())
-    .pipe(gulp.dest('dist/html/include'))
+	let source = 'src/html/include/*.html';
+	return gulp.src(source)
+		.pipe(html())
+		.pipe(gulp.dest('dist/html/include'))
 }
 
 function htmlPage() {
-  if (process.env.NODE_ENV == 'product') {
-    gulp.src('src/html/include/head.html')
-      .pipe(replace('bootstrap.css', 'bootstrap.min.css'))
-      .pipe(gulp.dest('src/html/include/'));
-  } else if (process.env.NODE_ENV == 'develope') {
-    gulp.src('src/html/include/head.html')
-      .pipe(replace('bootstrap.min.css', 'bootstrap.css'))
-      .pipe(gulp.dest('src/html/include/'));
-  }
-  return gulp.src('src/html/*.html')
-    .pipe(html())
-    .pipe(gulp.dest('dist/html/'))
+	if (process.env.NODE_ENV == 'product') {
+		gulp.src('src/html/include/head.html')
+			.pipe(replace('bootstrap.css', 'bootstrap.min.css'))
+			.pipe(gulp.dest('src/html/include/'));
+	} else if (process.env.NODE_ENV == 'develope') {
+		gulp.src('src/html/include/head.html')
+			.pipe(replace('bootstrap.min.css', 'bootstrap.css'))
+			.pipe(gulp.dest('src/html/include/'));
+	}
+	return gulp.src('src/html/*.html')
+		.pipe(html())
+		.pipe(gulp.dest('dist/html/'))
 }
 
 function copyCss() {
-  gulp.src(['src/css/dist/bootstrap.min.css', 'src/css/dist/bootstrap.min.css.map'])
-    .pipe(gulp.dest('dist/css'));
-  return gulp.src('src/css/dist/fonts/**',)
-    .pipe(gulp.dest('dist/css/fonts'));
+	gulp.src(['src/css/dist/bootstrap.min.css', 'src/css/dist/bootstrap.min.css.map'])
+		.pipe(gulp.dest('dist/css'));
+	return gulp.src('src/css/dist/fonts/**',)
+		.pipe(gulp.dest('dist/css/fonts'));
 }
 
 function copyImg() {
-  return gulp.src('src/img/**/**')
-    .pipe(gulp.dest('dist/img'));
+	return gulp.src('src/img/**/**')
+		.pipe(gulp.dest('dist/img'));
 }
 
 function copyFonts() {
-  return gulp.src('src/fonts/**/**')
-    .pipe(gulp.dest('dist/fonts'));
+	return gulp.src('src/fonts/**/**')
+		.pipe(gulp.dest('dist/fonts'));
 }
 
 function jsLib() {
-  let sourceLib = [
-    'src/js/src/jquery.js',
-    'src/js/src/bootstrap.min.js',
-    'src/js/src/bootstrap-datepicker.js',
-    'src/js/src/bootstrap-datepicker.ko.min.js'
-  ];
-  return gulp.src(sourceLib)
-    .pipe(concat('bundle.js'))
-    .pipe(gulp.dest('dist/js'))
+	let sourceLib = [
+		'src/js/src/jquery.js',
+		'src/js/src/bootstrap.min.js',
+		'src/js/src/bootstrap-datepicker.js',
+		'src/js/src/bootstrap-datepicker.ko.min.js',
+		'src/js/src/jquery.mCustomScrollbar.concat.min.js',
+	];
+	return gulp.src(sourceLib)
+		.pipe(concat('bundle.js'))
+		.pipe(gulp.dest('dist/js'))
 }
 
 function jsCommon() {
-  let sourceUi = ['src/js/ui/*.js'];
-  return gulp.src(sourceUi)
-    .pipe(concat('common.js'))
-    .pipe(gulp.dest('dist/js'))
+	let sourceUi = ['src/js/ui/*.js'];
+	return gulp.src(sourceUi)
+		.pipe(concat('common.js'))
+		.pipe(gulp.dest('dist/js'))
 }
 
 function watchScss() {
-  gulp.watch('src/scss/**/*.scss', gulp.series(scss));
+	gulp.watch('src/scss/**/*.scss', gulp.series(scss));
 }
 
 function watchHtml() {
-  gulp.watch(['src/html/*.html'], gulp.series(htmlPage));
+	gulp.watch(['src/html/*.html'], gulp.series(htmlPage));
 }
 
 function watchInclude() {
-  gulp.watch('src/html/include/*.html', gulp.series(htmlInclude, htmlPage));
+	gulp.watch('src/html/include/*.html', gulp.series(htmlInclude, htmlPage));
 }
 
 function watchJs() {
-  gulp.watch('src/js/*/*.js', gulp.series(jsLib, jsCommon));
+	gulp.watch('src/js/*/*.js', gulp.series(jsLib, jsCommon));
 }
 
 function watchImg() {
-  gulp.watch('src/img/**/*', gulp.series(copyImg));
+	gulp.watch('src/img/**/*', gulp.series(copyImg));
 }
 
 function watchFont() {
-  gulp.watch('src/fonts/**/**', gulp.series(copyFonts));
+	gulp.watch('src/fonts/**/**', gulp.series(copyFonts));
 }
 
 
 function beautify() {
-  var options = {
-    indentSize: 4
-  }
-  return gulp.src('./dist/html/*.html')
-    .pipe(htmlbeautify(options))
-    .pipe(gulp.dest('./dist/html/'))
+	var options = {
+		indentSize: 4
+	}
+	return gulp.src('./dist/html/*.html')
+		.pipe(htmlbeautify(options))
+		.pipe(gulp.dest('./dist/html/'))
 }
 
 function delDist() {
-  return del('dist');
+	return del('dist');
 }
 
 function setEnvProduct(cb) {
-  process.env.NODE_ENV = 'product';
-  cb();
+	process.env.NODE_ENV = 'product';
+	cb();
 }
 
 function setEnvDevelope(cb) {
-  process.env.NODE_ENV = 'develope'
-  cb();
+	process.env.NODE_ENV = 'develope'
+	cb();
 }
 
 
