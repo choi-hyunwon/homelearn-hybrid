cd6716a35a6e67da8762295f5e366761d749834b
Yujung Jung <darron@nate.com>
2020-09-02 19:40:05 +0900

    중간 백업, 저장 용

--- pagelist/data/data.js
+++ pagelist/data/data.js
@@ -1,97 +1,31 @@
 var data = {}
 data.common = {
-  'title': '공통',
-  'list': [
-    {
-      'stateClass': 'complete',
-      'url': 'LAYOUT',
-      'category': 'common',
-      'page': 'LAYOUT'
-    },
-    {
-      'stateClass': 'complete',
-      'url': 'COM_GENERAL_01',
-      'category': 'common',
-      'page': 'GENERAL_01'
-    },
-    {
-      'stateClass': 'complete',
-      'url': 'COM_GENERAL_02',
-      'category': 'common',
-      'page': 'GENERAL_02'
-    },
-    {
-      'stateClass': 'complete',
-      'url': 'COM_GENERAL_03',
-      'category': 'common',
-      'page': 'GENERAL_03'
-    },
-    {
-      'stateClass': 'complete',
-      'url': 'COM_modal_01',
-      'category': 'common',
-      'page': 'MODAL'
-    },
-    {
-      'stateClass': 'complete',
-      'url': 'COM_drawer_01',
-      'category': 'common',
-      'page': 'drawer'
-    },
-    {
-      'stateClass': 'complete',
-      'url': '00_common_01_profile_01',
-      'category': 'common',
-      'page': 'navigation'
-    },
-    {
-      'stateClass': 'complete',
-      'url': '00_common_01_popup_태그 알림 설정',
-      'category': 'common',
-      'page': '태그 알림 설정 팝업'
-    },
-    {
-      'stateClass': 'complete',
-      'url': '00_common_02_popup_언어설정',
-      'category': 'common',
-      'page': '언어 설정 팝업'
-    },
-    {
-      'stateClass': 'complete',
-      'url': '00_common_03_search_01_검색어 입력 전',
-      'category': 'common',
-      'page': '검색어 입력 전'
-    },
-    {
-      'stateClass': 'complete',
-      'url': '00_common_03_search_02_검색어 입력 시',
-      'category': 'common',
-      'page': '검색어 입력 시'
-    },
-    {
-      'stateClass': 'complete',
-      'url': '00_common_03_search_03_최근 검색어 없음',
-      'category': 'common',
-      'page': '최근 검색어 없음'
-    },
-    {
-      'stateClass': 'complete',
-      'url': '00_common_04_result_01_검색결과',
-      'category': 'common',
-      'page': '검색결과'
-    },
-    {
-      'stateClass': 'complete',
-      'url': '00_common_04_result_02_검색결과 없음',
-      'category': 'common',
-      'page': '검색결과 없음'
-    },
-    {
-      'stateClass': 'complete',
-      'url': '00_common_05_footer_01_이용약관',
-      'category': 'common',
-      'page': '이용약관'
-    },
-  ]
+	'title': '메인',
+	'list': [
+		{
+			'stateClass': 'complete',
+			'url': 'todayStudy',
+			'category': '에인',
+			'page': '메인_B2C_올인원'
+		},
+		{
+			'stateClass': 'complete',
+			'url': 'noStudyToday',
+			'category': '에인',
+			'page': '09_GNB_01_오늘의학습_01학습이없을때'
+		},
+		{
+			'stateClass': 'complete',
+			'url': 'myClassroom_noStudy',
+			'category': '에인',
+			'page': '01_메인_B2B_나의강의실_학습이없는경우'
+		},
+		{
+			'stateClass': 'working',
+			'url': 'mainStudyHall_college',
+			'category': '에인',
+			'page': '01_메인_학습관_단과'
+		}
+	]
 }
 
